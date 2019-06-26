53ac2dbfdfcbf1c3973b0ebce718e815d1e8d22e
juan-carlos-correa <juan.correa.herrera@gmail.com>
2019-06-25 21:48:39 -0500

    python_structural_composite | add CompositePizza for more complex example

--- python/structural/composite.py
+++ python/structural/composite.py
@@ -34,15 +34,39 @@ class Food(Menu):
         output = 'Name: %s, price: %s' % (self.name, self.price)
         print(output)
 
+class CompositePizza(Menu):
+    def __init__(self):
+        self._childrens = set()
+
+    def getValues(self):
+        for child in self._childrens:
+            child.getValues()
+
+    def add(self, menu):
+        self._childrens.add(menu)
+
+    def remove(self, menu):
+        self._childrens.discard(menu)
+
 def main():
     taco = Food(price=20, name='Taco')
-    pizza = Food(price=150, name='Pizza')
+    composite_pizza = composite_pizzas()
 
     composite_menu = CompositeMenu()
     composite_menu.add(taco)
-    composite_menu.add(pizza)
+    composite_menu.add(composite_pizza)
 
     composite_menu.getValues()
+
+def composite_pizzas():
+    pepperoni_pizza = Food(price=170, name='Pepperoni pizza')
+    hawaiiana_pizza = Food(price=160, name='Hawaiiana pizza')
+
+    composite_pizza = CompositePizza()
+    composite_pizza.add(pepperoni_pizza)
+    composite_pizza.add(hawaiiana_pizza)
+
+    return composite_pizza
 
 if __name__ == "__main__":
     main()
